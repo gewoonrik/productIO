0e693a8629aa4969cd1dc9d4cb1e05989fa45701
erwinvaneyk <erwinvaneyk@gmail.com>
2015-10-16 09:43:01 +0200

    Added homeserver-endpoint on the server

--- server/src/main/scala/ScalatraBootstrap.scala
+++ server/src/main/scala/ScalatraBootstrap.scala
@@ -1,10 +1,10 @@
 import nl.tudelft._
-import nl.tudelft.server.server
+import nl.tudelft.server.Server
 import org.scalatra._
 import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
-    context.mount(new server, "/api/")
+    context.mount(new Server, "/api/")
   }
 }
