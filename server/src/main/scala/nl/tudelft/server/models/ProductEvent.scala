1c430d86d2276d0dbbf3b49dacd525bc11e06b51
erwinvaneyk <erwinvaneyk@gmail.com>
2015-10-29 14:07:24 +0100

    Added readable timestamps to product (hack)

--- server/src/main/scala/nl/tudelft/server/models/ProductEvent.scala
+++ server/src/main/scala/nl/tudelft/server/models/ProductEvent.scala
@@ -9,17 +9,23 @@ import com.novus.salat._
 import com.novus.salat.annotations._
 import com.novus.salat.global._
 
-case class ProductEvent(_id: ObjectId = new ObjectId, productid : String, time : BSONTimestamp, event : String, user : String) extends Event
+case class ProductEvent(_id: ObjectId = new ObjectId, productid : String, time : BSONTimestamp,
+                        timestamp : Option[Long], event : String, user : String) extends Event {
+}
 
 object ProductEventDAO extends SalatDAO[ProductEvent, ObjectId](
   collection = ApiServer.mongoClient("productio")("products"))
 
 object ProductEvent {
-  def all(): List[ProductEvent] = ProductEventDAO.find(MongoDBObject.empty).toList
+  def all(): List[ProductEvent] = addTimestamps(ProductEventDAO.find(MongoDBObject.empty).toList)
 
   def create(time: BSONTimestamp, productid : String, event : String, user : String): (ProductEvent, ObjectId) = {
-    val product = ProductEvent(time = time, event = event, productid = productid, user = user)
+    val product = ProductEvent(time = time, timestamp = Some(time.getTime), event = event, productid = productid, user = user)
     val mongoId = ProductEventDAO.insert(product).get
     (product, mongoId)
+  }
+
+  def addTimestamps(products : List[ProductEvent]): List[ProductEvent] = {
+    products.map(product => product.copy(timestamp = Some(product.time.getTime)))
   }
 }
