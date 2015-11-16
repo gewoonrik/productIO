68d7cf5a2e355cf02f84c251d475b729aba8b21f
erwinvaneyk <erwinvaneyk@gmail.com>
2015-11-16 21:44:09 +0100

    initial predictor (start)

--- scripts/roommateDistribution.R
+++ scripts/roommateDistribution.R
@@ -6,9 +6,10 @@ roommateWeekDist = weightedRoommate;
 roommateWeekDist$day = weekdays(roommateWeekDist$date)
 
 roommateDayDist = aggregate(roommateWeekDist$count, list(day = roommateWeekDist$day), mean)
-
-write.csv(roommateDayDist, "~/Github/productIO/scripts/data/roommateDistribution.csv")
+roommateDayDist$std = aggregate(roommateWeekDist$count, list(day = roommateWeekDist$day), sd)$x
 
 plot(roommateDayDist$x, type="h",  axes=FALSE, xlab = "", ylim = c(0,1), main = "Roommates at home during breakfast/lunch", ylab = "Roommate Factor")
 axis(2)
 axis(1, at=seq_along(roommateDayDist$x),labels=as.character(roommateDayDist$day), las=2)
+
+write.csv(roommateDayDist, "~/Github/productIO/scripts/data/roommateDistribution.csv")
