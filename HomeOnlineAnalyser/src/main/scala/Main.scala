7e9c43bd8b8675234fabbc64c5ee53d9a9906cd1
Rik Nijessen <riknijessen@gmail.com>
2015-11-23 17:30:34 +0100

    implemented real distinct people at home per hour

--- HomeOnlineAnalyser/src/main/scala/Main.scala
+++ HomeOnlineAnalyser/src/main/scala/Main.scala
@@ -15,39 +15,71 @@ object Main {
         }).toMap
 
 
+
     println(Day.getDays
       .groupBy(day => day.realDay.getDayOfWeek)
       .map(
-        tuple => (tuple._1, tuple._2.map(day => Math.round(day.getAveragePeopleHomeBetween(7, 9)*100)/100.0))
+        tuple => (tuple._1, tuple._2.map(day => day.getDistinctPeopleHomeBetween(7, 14)))
       ))
 
-    val breads = Bread.getBreads
 
-    println(Bread.getBreads
-      .groupBy(bread => bread.getDateTimeIn.getDayOfWeek)
-      .map {
-      case (day, breads_) =>
-        (day, breads_.map(bread => bread.getDaysEaten.size))
-      })
 
-    val manHoursPerBread =
-      //get al breads that have online information for all days the bread has been eaten
+    /* println(Day.getDays
+       .groupBy(day => day.realDay.getDayOfWeek)
+       .map(
+         tuple => (tuple._1, tuple._2.map(day => Math.round(day.getAveragePeopleHomeBetween(7, 9)*100)/100.0))
+       ))
+*/
+     val breads = Bread.getBreads
+    val manHoursPerBreadDistinct =
+    //get al breads that have online information for all days the bread has been eaten
       breads.filter(bread =>
         bread.getDaysEaten.forall(day => days.get(new LocalDate(day)).isDefined)
       )
-      //map them to manhours
-      .map(bread => {
+        //map them to manhours
+        .map(bread => {
         bread.getDaysEaten
           //get the corresponding online data for each day
           .map(day => days.get(new LocalDate(day)).get)
           //for each day calculate the manhours eating that day
           .map(day => {
-            day.getAveragePeopleHomeBetween(7, 9)
-          })
+          day.getDistinctPeopleHomeBetween(7, 14)
+        })
           .sum
       }
-      )
-    println(manHoursPerBread)
+        )
 
+    println(manHoursPerBreadDistinct)
+
+
+
+    //calculate days between check-in and check-out grouped by day of week
+    println("days between check-in and check-out grouped by day of week ")
+    println(Bread.getBreads
+       .groupBy(bread => bread.getDateTimeIn.getDayOfWeek)
+       .map {
+       case (day, breads_) =>
+         (day, breads_.map(bread => bread.getDaysEaten.size))
+       })
+    /*
+         val manHoursPerBread =
+           //get al breads that have online information for all days the bread has been eaten
+           breads.filter(bread =>
+             bread.getDaysEaten.forall(day => days.get(new LocalDate(day)).isDefined)
+           )
+           //map them to manhours
+           .map(bread => {
+             bread.getDaysEaten
+               //get the corresponding online data for each day
+               .map(day => days.get(new LocalDate(day)).get)
+               //for each day calculate the manhours eating that day
+               .map(day => {
+                 day.getAveragePeopleHomeBetween(7, 14)
+               })
+               .sum
+           }
+           )
+         println(manHoursPerBread)
+     */
   }
 }
--- HomeOnlineAnalyser/src/main/scala/Online/Day.scala
+++ HomeOnlineAnalyser/src/main/scala/Online/Day.scala
@@ -7,26 +7,58 @@ import org.joda.time.LocalDate
 /**
  * Created by rik on 17/11/15.
  */
-case class Day(realDay: LocalDate, hourToPersons : Map[Int, Int]) {
+case class Day(realDay: LocalDate, onlineData: List[OnlineData]) { /*hourToPersons : Map[Int, Int]) {*/
+  val validMacs = List("40:F3:08:F2:9B:79", "78:6C:1C:C7:05:46", "88:C9:D0:EB:22:AF", "BC:F5:AC:FB:31:71")
+
+
+  private def getPeopleHomePerHour() : Map[Int, Int] = {
+    onlineData
+      .groupBy(data => data.getDateTime.getHourOfDay)
+      .map({
+      case (hour, data: List[OnlineData]) => {
+        hour -> data.flatMap(
+          data => data.getMacs
+            .filter(mac => validMacs.contains(mac))
+        ).distinct.size
+      }
+    })
+  }
+
+  //returns the macs of mobile phones online, per hour
+  private def getMobileMacsPerHour() : Map[Int, List[String]] = {
+    onlineData
+      .groupBy(data => data.getDateTime.getHourOfDay)
+      .map({
+      case (hour, data: List[OnlineData]) => {
+        hour -> data.flatMap(
+          data => data.getMacs
+            .filter(mac => validMacs.contains(mac))
+        ).distinct
+      }
+    })
+  }
 
   // not including endHour, but including startHour
   def getAveragePeopleHomeBetween(startHour: Int, endHour: Int): Double = {
+    val peopleHomePerHour = getPeopleHomePerHour
+
     val peopleHome =
       (startHour until endHour)
-        .filter(hourToPersons.isDefinedAt)
-        .map(hourToPersons.get)
+        .filter(peopleHomePerHour.isDefinedAt)
+        .map(peopleHomePerHour.get)
         .map(_.get)
 
     peopleHome.sum/peopleHome.size.asInstanceOf[Double]
   }
 
-  def getDistinctPeopleHomeBetween(startHour: Int, endHour: Int): Double = {
+  def getDistinctPeopleHomeBetween(startHour: Int, endHour: Int): Int = {
+    val macsPerHour = getMobileMacsPerHour
       (startHour until endHour)
-        .filter(hourToPersons.isDefinedAt)
-        .map(hourToPersons.get)
-        .map(_.get)
-        .max
-
+        .filter(macsPerHour.isDefinedAt)
+        .map(macsPerHour.get)
+        .flatMap(_.get)
+        .distinct
+        .size
   }
 }
 
@@ -50,22 +82,10 @@ object Day {
       .groupBy(data => new LocalDate(data.getDateTime))
       .map {
       case (day, data: List[OnlineData]) => {
-        Day(day, dataToMapOfHoursNrOfPeople(data))
+        Day(day, data)
       }
     }.toList
   }
 
-  private def dataToMapOfHoursNrOfPeople(data: List[OnlineData]) : Map[Int, Int] = {
-    val validMacs = List("40:F3:08:F2:9B:79", "78:6C:1C:C7:05:46", "88:C9:D0:EB:22:AF", "BC:F5:AC:FB:31:71")
-    data
-      .groupBy(data => data.getDateTime.getHourOfDay)
-      .map({
-        case (hour, data: List[OnlineData]) => {
-          hour -> data.flatMap(
-            data => data.getMacs
-              .filter(mac => validMacs.contains(mac))
-          ).distinct.size
-        }
-      })
-  }
+
 }
